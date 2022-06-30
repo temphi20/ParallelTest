fd6e1fabc2709115ed02e1b75b697f4329eb5377
Joohyun Jo <jh.jo@wgslabs.com>
2022-06-30 14:15:52 +0900

    set

---
+++ ParallelTest/framework.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+typedef double DOUBLE;
+typedef int INT;
+typedef long LONG;
+typedef long long int LLONG;
+
+typedef unsigned int UINT;
+typedef unsigned long ULONG;
+typedef unsigned long long int ULLONG;
+
+const ULONG TEST_LNUM = 4294967295;
+const ULLONG TEST_LLNUM = 18446744073709551615;
